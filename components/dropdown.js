17da25d4d0a3e2503a20da96a6e9700ca865fd35
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-06-22 09:56:10 +0100

    fixed drop dropdown

--- components/dropdown.js
+++ components/dropdown.js
@@ -20,7 +20,7 @@ export default function Dropdown({ label, options, onClick }) {
   const openedDrop = closedDrop + " hidden";
 
   return (
-    <div className="flex flex-col sm:mb-14">
+    <div className="flex flex-col sm:mb-14 mb-8">
       <div className="flex justify-between items-center border-2 p-3 sm:w-80 w-30 sm:h-16 sm:h-10">
         <p className="home-p">{label}</p>
         <button
