56d1a527ecafbafca8c6409d62c5f79bfcb00754
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-11-13 13:27:28 +0100

    feat: added animation to dropdown

--- components/dropdown.js
+++ components/dropdown.js
@@ -15,9 +15,11 @@ export default function Dropdown({ label, options, onClick }) {
     setIsOpen(!isOpen);
   };
 
-  const closedDrop =
-    "block w-full p-3 font-normal text-2xl hover:bg-yellow-300 hover:text-black";
-  const openedDrop = closedDrop + " hidden";
+  const openedDrop = "block ";
+  const closedDrop = " hidden";
+
+  const closed = " -translate-y-10 opacity-0";
+  const opened = " translate-y-0 opacity-100";
 
   return (
     <div className=" relative mb-8 flex w-44 flex-col lg:mb-14 lg:w-80">
@@ -31,20 +33,28 @@ export default function Dropdown({ label, options, onClick }) {
             src={Arrow}
             alt=""
             className={
-              (!isOpen ? "rotate-180" : "rotate-0") +
-              " z-0 h-3 w-3 lg:h-5 lg:w-5"
+              (!isOpen ? "rotate-0" : "rotate-180") +
+              " transition-tranform z-0 h-3 w-3 opacity-100 duration-300 ease-in-out md:opacity-70 md:hover:opacity-100 lg:h-5 lg:w-5"
             }
           />
         </button>
       </div>
 
-      <div className="absolute top-14 z-10 w-full border-2 border-t-0 bg-black lg:top-16">
+      <div
+        className={
+          "transition-tranform absolute top-14 z-10 w-full border-2 border-t-0 bg-black duration-300 ease-out lg:top-16" +
+          (isOpen ? opened : closed)
+        }
+      >
         {options.map((option) => (
           <button
             value={option}
             onClick={onClick}
             key={option}
-            className={isOpen ? openedDrop : closedDrop}
+            className={
+              " w-full p-3 text-2xl font-normal hover:bg-yellow-300 hover:text-black " +
+              (isOpen ? openedDrop : closedDrop)
+            }
           >
             {option}
           </button>
